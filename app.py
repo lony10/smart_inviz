339a3f174a7f1c2b72a07a57ba1644941e447a50
lony10 <154953863+lony10@users.noreply.github.com>
2025-04-21 00:06:28 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -374,5 +374,6 @@ def get_alerts():
     return jsonify(response_alerts)
 
 
+# For Render deployment
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=False)
